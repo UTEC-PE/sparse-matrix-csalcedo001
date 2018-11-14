8254ff7ef6399e273c9dda909d33467fef53a14d
César Salcedo <cesar.salcedo@utec.edu.pe>
2018-11-13 20:21:57 -0500

    Added all methods of Matrix

--- main.cpp
+++ main.cpp
@@ -2,19 +2,52 @@
 
 #include "matrix.h"
 
-#define WIDTH 4
+#define WIDTH 2
 #define HEIGHT 3
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    Matrix<int> a(WIDTH, HEIGHT);
+    Matrix<int> a(WIDTH, HEIGHT), b(HEIGHT, WIDTH);
+
+    a.iterator([&] (int x, int y) -> void {
+      cout << a(x, y) << endl;
+    }, 'n', 'd');
 
     a.iterator([&a] (int x, int y) {
       a.set(x, y, x + y * WIDTH);
     });
+    b.iterator([&b] (int x, int y) {
+      b.set(x, y, x + y * HEIGHT);
+    });
 
     a.print();
+    b.print();
+
+    Matrix<int> c;
+    c = a * b;
+
+    c.print();
+
+    c = c * 4;
+
+    c.print();
+
+    Matrix<int> d = c;
+
+    d = c + c + c;
+
+    d.print();
+
+    d = d - c;
+
+    d.print();
+
+    d = d.transposed();
+
+    d.print();
+
+    cout << endl << endl;
 
     system("PAUSE");
     return EXIT_SUCCESS;
--- node.h
+++ node.h
@@ -8,9 +8,10 @@ class Node {
 public:
     int x, y;
     T data;
-    Node* next;
-    Node* down;
-    Node <T> () : next(nullptr), down(nullptr) {};
+    Node <T> *next;
+    Node <T> *down;
+
+    Node <T> () : x(-1), y(-1), data(0), next(nullptr), down(nullptr) {};
     Node <T> (int x, int y, T data) : data(data), x(x), y(y), next(nullptr), down(nullptr) {};
 
     void killSelf() {
